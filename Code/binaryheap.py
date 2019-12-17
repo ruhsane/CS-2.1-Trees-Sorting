aa7c23cdf9343f012929d85cd13703ec7b7fdb73
Ruhsane <ruhsane924@gmail.com>
2019-12-16 21:52:47 -0800

    heapify and heap sort correct indentation

--- Code/binaryheap.py
+++ Code/binaryheap.py
@@ -148,40 +148,42 @@ class BinaryMinHeap(object):
         """Return the right child index of the item at the given index."""
         return (index << 1) + 2  # Shift left to multiply by 2
 
-    def heapify(arr, n, i): 
-        largest = i # Initialize largest as root 
-        left = 2 * i + 1
-        right = 2 * i + 2
-    
-        # See if left child of root exists and is 
-        # greater than root 
-        if left < n and arr[i] < arr[left]: 
-            largest = left
-    
-        # See if right child of root exists and is 
-        # greater than root 
-        if right < n and arr[largest] < arr[right]: 
-            largest = right
-    
-        # Change root, if needed 
-        if largest != i: 
-            arr[i],arr[largest] = arr[largest],arr[i] # swap 
-    
-            # Heapify the root. 
-            heapify(arr, n, largest) 
-  
-    # The main function to sort an array of given size 
-    def heap_sort(arr): 
-        n = len(arr) 
-    
-        # Build a maxheap. 
-        for i in range(n, -1, -1): 
-            heapify(arr, n, i) 
-    
-        # One by one extract elements 
-        for i in range(n-1, 0, -1): 
-            arr[i], arr[0] = arr[0], arr[i] # swap 
-            heapify(arr, i, 0) 
+def heapify(arr, n, i): 
+    largest = i # Initialize largest as root 
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    # See if left child of root exists and is 
+    # greater than root 
+    if left < n and arr[i] < arr[left]: 
+        largest = left
+
+    # See if right child of root exists and is 
+    # greater than root 
+    if right < n and arr[largest] < arr[right]: 
+        largest = right
+
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] # swap 
+
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+
+# The main function to sort an array of given size 
+def heap_sort(arr): 
+    n = len(arr) 
+
+    # Build a maxheap. 
+    for i in range(n, -1, -1): 
+        heapify(arr, n, i) 
+
+    # One by one extract elements 
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i] # swap 
+        heapify(arr, i, 0) 
+
+    return arr
 
 
 def test_binary_min_heap():
@@ -211,3 +213,4 @@ def test_binary_min_heap():
 
 if __name__ == '__main__':
     test_binary_min_heap()
+    print(heap_sort([2,6,1,7,3,8,3,3,0]))
